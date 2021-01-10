f8944aa21985d0411cfeae723fb55535e269d9fb
Adhithya Kondalsamy <adhithyaky@gmail.com>
2021-01-09 23:43:12 -0500

    Started working on login and register page functionality

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortenedUrl')
 const app = express()
+const bcrpyt = require('bcrypt')
+
+const users = []
 
 mongoose.connect('mongodb://localhost/urlShortener', {
     useNewUrlParser: true, useUnifiedTopology: true
@@ -10,14 +13,50 @@ mongoose.connect('mongodb://localhost/urlShortener', {
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', async (req,res) => {
+app.get('/', (req, res) => {
+    res.render('index.ejs', { name: 'Adhithya' })
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login'), (req, res) =>{
+    
+}
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', async (req, res) => {
+    try {
+        //console.log("hello")
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        console.log(hashedPassword)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        //console.log(users)
+        res.redirect('/register')
+    }
+    //console.log(users)
+    
+})
+
+app.get('/url-shortener', async (req,res) => {
     const shortenedUrls = await ShortUrl.find()
-    res.render('index', { shortenedUrls: shortenedUrls })
+    res.render('urlShortener.ejs', { shortenedUrls: shortenedUrls })
 })
 
 app.post('/shortenedURLs', async (req,res) => {
     await ShortUrl.create({ unshortened: req.body.unshortenedURL })
-    res.redirect('/')
+    res.redirect('/url-shortener')
 })
 
 app.get('/:shortenedUrl', async (req, res) => {
